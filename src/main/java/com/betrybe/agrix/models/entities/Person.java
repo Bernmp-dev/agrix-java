b43df2f7b177247cc6572ef86db8532ddc21ce61
eon <bernardo.marquesp@gmail.com>
2023-10-17 15:42:47 -0300

    Refactor: Person implements UserDetails, GrantedAuthority.

---
+++ src/main/java/com/betrybe/agrix/models/entities/Person.java
@@ -0,0 +1,127 @@
+package com.betrybe.agrix.models.entities;
+
+
+import com.betrybe.agrix.dto.PersonDto;
+import com.betrybe.agrix.security.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Class representing a person.
+ */
+@Entity
+@Table(name = "persons")
+public class Person implements UserDetails, GrantedAuthority {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(unique = true)
+  private String username;
+  private String password;
+  private Role role;
+
+  public Person() {
+  }
+
+  /** Person constructor. */
+  public Person(String username, String password, Role role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+
+  public PersonDto toDto() {
+    return new PersonDto(id, username, role);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @JsonIgnore
+  @Override
+  public Collection<Person> getAuthorities() {
+    return List.of(this);
+  }
+
+  @JsonIgnore
+  @Override
+  public String getAuthority() {
+    return this.getRole().getAuthority();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Person person = (Person) o;
+    return Objects.equals(id, person.id) && Objects.equals(username,
+      person.username) && Objects.equals(password, person.password)
+      && Objects.equals(role, person.role);
+  }
+}
