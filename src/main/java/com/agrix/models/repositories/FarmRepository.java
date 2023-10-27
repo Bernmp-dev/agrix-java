7f18261b70da1fb6caa6fc59913831eb605f3f4a
eon <bernardo.marquesp@gmail.com>
2023-10-27 17:51:52 -0300

    [FarmRepository]Feat: findByName .

--- src/main/java/com/agrix/models/repositories/FarmRepository.java
+++ src/main/java/com/agrix/models/repositories/FarmRepository.java
@@ -2,7 +2,10 @@ package com.agrix.models.repositories;
 
 import com.agrix.models.entities.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 /** Farm Repository. */
 public interface FarmRepository extends JpaRepository<Farm, Long> {
+  
+  Optional<Farm> findByName(String name);
 }
