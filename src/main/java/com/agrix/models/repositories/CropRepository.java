ec010c9413e737752bc6b57223a363d501aae490
eon <bernardo.marquesp@gmail.com>
2023-11-07 16:49:00 -0300

    [CropRepository]Feat: findFirstByOrderByIdDesc .

--- src/main/java/com/agrix/models/repositories/CropRepository.java
+++ src/main/java/com/agrix/models/repositories/CropRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /** Crop Repository. */
 public interface CropRepository extends JpaRepository<Crop, Long> {
   List<Crop> findByHarvestDateBetween(LocalDate start, LocalDate end);
+
+  Crop findFirstByOrderByIdDesc();
 }
